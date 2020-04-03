05d07539da9ad4013a3be33ce8fc72353ddbc7d7
Jeff Cho <jeffcho0227@gmail.com>
2020-04-02 19:56:21 -0700

    feat: BMI app - added slide and change slider value to the app

--- BMI_calculator/BMI Calculator/Controllers/ViewController.swift
+++ BMI_calculator/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var HeightLabel: UILabel!
+    @IBOutlet weak var WeightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value);
+        HeightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        WeightLabel.text = "\(weight)Kg"
+    }
 }
 
