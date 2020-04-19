fb1622f89f92411c9e23b1f75c1b0858889642a2
Jeff Cho <jeffcho0227@gmail.com>
2020-04-18 17:45:51 -0700

    feat: setup BMI outlest to change while change the slider's value

--- BMI_calculator/BMI Calculator/Controllers/ViewController.swift
+++ BMI_calculator/BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value)
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f", sender.value)
+    }
 }
 
